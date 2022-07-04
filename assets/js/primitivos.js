8b3d525949525c3fd5d04334c35244041eb940b4
Bryan Jimenez <bryanbg.jg@gmail.com>
2022-07-03 22:48:47 -0400

    se agregaron tipos de variables

---
+++ assets/js/primitivos.js
@@ -0,0 +1,23 @@
+
+//string
+let nombre = 'Bryan Jimenez';
+console.log(nombre);
+
+nombre = 'Alexander guerrero';
+console.log(nombre);
+
+nombre = "Trey";
+nombre = `Bryan Trey`;
+console.log(typeof nombre);
+
+//number
+let numero = 12345;
+console.log(typeof numero);
+
+//bool
+let boleano = true;
+console.log(typeof boleano);
+
+//symbol
+let symbol1 = Symbol('a');
+console.log(typeof symbol1);
