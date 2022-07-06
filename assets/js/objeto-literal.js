abe63c59126d5002165d9347967ffbd7750d7c1d
Bryan Jimenez <bryanbg.jg@gmail.com>
2022-07-05 22:47:59 -0400

    Se agrego lo basico de los objetos literales en js

---
+++ assets/js/objeto-literal.js
@@ -0,0 +1,55 @@
+const personaje = {
+    nombre: 'Tony Stark',
+    codeName: 'Iron Man',
+    vivo: false,
+    edad: 40,
+    coords: {
+        lat: 34.034,
+        lng: -118.70
+    },
+    trajes: ['Mark I', 'Mark V', 'Hulkbuster'],
+    direccion: {
+        zip: '10880, 90265',
+        ubicacion: 'Malibu, California'
+    }
+
+};
+
+console.log(personaje);
+console.log('Nombre:', personaje.nombre);
+console.log('Edad:', personaje.edad);
+
+//Ejercicio 
+
+console.log('No.Trajes:', personaje.trajes.length);
+
+//Mas detalles
+
+//Elimando
+
+delete personaje.edad;
+console.log(personaje);
+
+personaje.casado =  true;
+
+//Pares de valores
+
+const entriesPares = Object.entries(personaje);
+console.log(entriesPares);
+
+//Utilizando el freeze
+
+Object.freeze(personaje);
+
+personaje.nombre = 'Sr. Stark';
+console.log(personaje);
+
+
+//Obteniendo las propiedas en un array
+
+const propiedades = Object.getOwnPropertyNames(personaje);
+
+//Obteniendo los valores
+
+const valores = Object.values(personaje);
+console.log({propiedades, valores});
